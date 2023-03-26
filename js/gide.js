7aa791f608ac347a8431ab5e1cafa75c5cde1963
dydtka369 <dydtka369@naver.com>
2023-03-26 22:36:50 +0900

    로그인 수정본

--- js/gide.js
+++ js/gide.js
@@ -7,12 +7,13 @@ $(function(){
             $('.intr_pic1').css('animation-name','none')
         }
         if(pos>500){
-            $('.intr_pic2').css('animation-name','gide2')
             $('.intr_pic3').css('animation-name','gide2')
+            $('.intr_pic2').css('animation-name','gide2')
          }else if(pos<600) {
              $('.intr_pic2').css('animation-name','none')
              $('.intr_pic3').css('animation-name','none')
          }
+         console.log(pos)
     })
     let index;
     $('.selected').click(function(){
